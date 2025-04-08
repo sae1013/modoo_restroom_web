5e0bc3f1e2a736ab3fbc6d8d9f63e17313f17e46
정민우 소프트웍스 <sae1013@gmail.com>
2025-04-09 00:38:54 +0900

    [bugfix] 바텀시트 가변높이 IOS 대응 (초기값 0 애니메이션 대응)

--- src/components/bottomsheet/BottomSheet.tsx
+++ src/components/bottomsheet/BottomSheet.tsx
@@ -3,17 +3,19 @@ import styled from '@emotion/styled';
 import { motion } from 'framer-motion';
 
 const Wrapper = styled(motion.div)`
-    display: flex;
-    flex-direction: column;
-    position: fixed;
-    left: 0;
-    width: 100%;
-    bottom: 0;
-    border-top-left-radius: 8px;
-    border-top-right-radius: 8px;
-    background-color: #fff;
-    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.6);
-    overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+
+  /* bottom: 0; */
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  background-color: #fff;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.6);
+  overflow: hidden;
 `;
 
 interface BottomSheetProps {
@@ -22,7 +24,7 @@ interface BottomSheetProps {
 }
 
 // 애니메이션을 자연스럽게 하기위한 조정값
-const SMOOTH_BOTTOM_OFFSET = 30;
+const SMOOTH_BOTTOM_OFFSET = 20;
 
 const BottomSheet = ({ children, onCloseCallback, ...props }: BottomSheetProps) => {
   const wrapperRef = useRef<HTMLDivElement>(null);
@@ -36,19 +38,28 @@ const BottomSheet = ({ children, onCloseCallback, ...props }: BottomSheetProps) 
 
   useLayoutEffect(() => {
     // 콘텐츠 컨테이너의 높이를 측정하여 targetHeight에 저장
-    const maxHeight = window.visualViewport.height - 10;
+    const maxHeight = window?.visualViewport?.height || 0 - 10;
     if (wrapperRef.current) {
       setWrapperHeight(Math.min(wrapperRef.current.clientHeight, maxHeight));
     }
   }, []);
-  // console.log('wrapperHeight', wrapperHeight);
+  const variantOption = { hidden: { y: wrapperHeight }, visible: { y: 0 }, exit: { y: wrapperHeight + SMOOTH_BOTTOM_OFFSET } };
+
+  if (wrapperHeight <= 0) {
+    return (
+      <div ref={wrapperRef} style={{ visibility: 'hidden' }}>
+        {children}
+      </div>
+    );
+  }
   return (
     <Wrapper
       {...props}
       ref={wrapperRef}
-      initial={{ bottom: -wrapperHeight }}
-      animate={{ bottom: 0 }}
-      // exit={{ y: window.innerHeight + SMOOTH_BOTTOM_OFFSET }}
+      variants={variantOption}
+      initial="hidden" // 초기 상태 지정
+      animate="visible" // 나타날 때 상태 지정
+      exit="exit"
       transition={{ type: 'spring', stiffness: 250, damping: 30 }}
     >
       {header}
