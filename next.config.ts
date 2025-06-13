abde37ea01be26552431eeff4747e9410290c21d
정민우 소프트웍스 <sae1013@gmail.com>
2025-06-13 17:49:05 +0900

    next config 설정 변경

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'standalone',
   /* config options here */
+  reactStrictMode: false,
   compiler: {
     styledComponents: true,
   },
