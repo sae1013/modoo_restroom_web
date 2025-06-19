7f4c38af5986d51b6670fba16b909edf2148b83e
정민우 소프트웍스 <sae1013@gmail.com>
2025-06-19 17:43:09 +0900

    [FEAT] 모달 작업 시, 자체 크기만큼 움직이도록 수정

--- src/components/bottomsheet/BottomSheet.tsx
+++ src/components/bottomsheet/BottomSheet.tsx
@@ -2,20 +2,22 @@
 import React, { ComponentType, ReactNode, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import styled from '@emotion/styled';
 import { motion } from 'framer-motion';
+import { css } from '@styled-system/css';
 
 const Wrapper = styled(motion.div)<{ maxHeight: number }>`
-    display: flex;
-    flex-direction: column;
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-        //max-height: ${({ maxHeight }) => `${maxHeight}px`}; /* ← 동적 max-height */
-    border-top-left-radius: 8px;
-    border-top-right-radius: 8px;
-    background-color: #fff;
-    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.6);
-    overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  max-height: 90vh;
+  //max-height: ${({ maxHeight }) => `${maxHeight}px`}; /* ← 동적 max-height */
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  background-color: #fff;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.6);
+  overflow: hidden;
 `;
 
 interface BottomSheetProps {
@@ -28,7 +30,7 @@ const SMOOTH_BOTTOM_OFFSET = 70;
 
 const BottomSheet = ({ children, onCloseCallback, ...props }: BottomSheetProps) => {
   const wrapperRef = useRef<HTMLDivElement>(null);
-  const [wrapperHeight, setWrapperHeight] = useState(0);
+  const [wrapperHeight, setWrapperHeight] = useState();
 
   const childrenArray = React.Children.toArray(children);
 
@@ -45,9 +47,12 @@ const BottomSheet = ({ children, onCloseCallback, ...props }: BottomSheetProps) 
     }
   }, []);
   const variantOption = {
-    hidden: { y: wrapperHeight },
+    // hidden: { y: wrapperHeight },
+    // visible: { y: 0 },
+    // exit: { y: wrapperHeight + SMOOTH_BOTTOM_OFFSET },
+    hidden: { y: '100%' },
     visible: { y: 0 },
-    exit: { y: wrapperHeight + SMOOTH_BOTTOM_OFFSET },
+    exit: { y: '100%' },
   };
 
   // if (wrapperHeight <= 0) {
@@ -60,7 +65,7 @@ const BottomSheet = ({ children, onCloseCallback, ...props }: BottomSheetProps) 
   return (
     <Wrapper
       {...props}
-      maxHeight={wrapperHeight}
+      // maxHeight={wrapperHeight}
       ref={wrapperRef}
       variants={variantOption}
       initial="hidden" // 초기 상태 지정
@@ -68,9 +73,28 @@ const BottomSheet = ({ children, onCloseCallback, ...props }: BottomSheetProps) 
       exit="exit"
       transition={{ type: 'spring', stiffness: 250, damping: 30 }}
     >
-      {header}
-      {contents}
-      {footer}
+      <div
+        className={css({
+          flex: 1,
+        })}
+      >
+        {header}
+      </div>
+      <div
+        className={css({
+          flex: 9,
+          overflow: 'scroll',
+        })}
+      >
+        {contents}
+      </div>
+      <div
+        className={css({
+          flex: 1,
+        })}
+      >
+        {footer}
+      </div>
     </Wrapper>
   );
 };
